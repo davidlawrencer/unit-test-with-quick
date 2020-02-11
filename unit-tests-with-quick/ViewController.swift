5f5ff048e8883b2a6199a5813a5622ecb6b438e2
davidrifkin <davidlrifkin1@gmail.com>
2020-02-11 13:14:15 -0500

    Initial Commit

---
+++ unit-tests-with-quick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  unit-tests-with-quick
+//
+//  Created by David Rifkin on 2/11/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
